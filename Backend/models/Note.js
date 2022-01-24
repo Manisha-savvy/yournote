6b9907e57d9653b09303d38953cfd2a08669c815
Manisha <manisha.iitkgp33@gmail.com>
2022-01-24 14:44:15 +0530

    Backend Complete

--- Backend/models/Note.js
+++ Backend/models/Note.js
@@ -3,18 +3,23 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
   const notesSchema = new Schema({
+    user:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user'
+    },
+    
     title:{
-        type:string,
+        type:String,
         required:true
     },
 
     description:{
-        type:string,
+        type:String,
         required:true,
         unique:true
     },
     tag:{
-        type:string,
+        type:String,
         default: "General"
     },
     date:{
--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,9 +1,94 @@
 const express = require('express')
 const router = express.Router();
+var fetchuser = require('../middleware/fetchuser');
+const Note = require('../models/Note');
+const { body, validationResult } = require('express-validator');
 
-router.get('/',(req,res)=>{
-    
-    res.json([]);
+
+//Route 1: Get all the notes using: POST "/api/notes/fetchallnotes". Login required.
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error Occured");
+
+    }
+
 })
 
+//Route 2: Add a new note using: POST "/api/notes/addnote". Login required.
+router.post('/addnote', fetchuser, [
+    body('title', "Please ensure title is atleast 3 charcters").isLength({ min: 3 }),
+    body('description', "Please ensure title is atleast 5 charcters").isLength({ min: 5 })
+], async (req, res) => {
+    try {
+
+        const { title, description, tag } = req.body;
+
+        // If errors return badrequest and the error
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const note = new Note({
+            title, description, tag, user: req.user.id
+        })
+        const savedNote = await note.save()
+        res.json(savedNote);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error Occured");
+    }
+})
+
+//Route 3: Update existing note using: PUT "/api/notes/updatenote". Login required.
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error Occured");
+    }
+})
+
+//Route 4: Delete note using: DELETE "/api/notes/updatenote". Login required.
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+
+        // Find the note to be deleted and delete it
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") }
+
+        // Allow deletion only if the user owns this Note
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+        note = await Note.findByIdAndDelete(req.params.id)
+        res.json({ "Sucsess": "Note have been deleted", note: note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error Occured");
+    }
+})
 module.exports = router
